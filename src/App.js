9b6a999739944c1fc19e686247c1f43b85d11e18
milymax <vararakya@gmail.com>
2022-04-15 01:27:54 +0700

    baru

--- src/App.js
+++ src/App.js
@@ -5,29 +5,24 @@ import Minuman from './pages/minuman';
 import About from './pages/about';
 import './App.css';
 import Home from './pages/home';
+import { getDefaultNormalizer } from '@testing-library/react';
 
 function App() {
   return (
     <div class="body">
-    <div class="container">
-      <nav class="navbar">
-        <div class="container">
+      <div className='navbar'>
           <a class="navbar-brand" href="/">KopiKita</a>
-        <div class="">
           <Link
-              class="fw-bold font-monospace text-decoration-none w-25 p-3 text-light" to="/promo">Promo</Link>
+        class="navbar" to="/promo">Promo</Link>
           <Link
-              class="fw-bold font-monospace text-decoration-none w-25 p-3 text-light" to="/makanan">Makanan</Link>
+        class="navbar" to="/makanan">Makanan</Link>
           <Link
-              class="fw-bold font-monospace text-decoration-none w-25 p-3 text-light" to="/minuman">Minuman</Link>
+        class="navbar" to="/minuman">Minuman</Link>
           <Link
-              class="fw-bold font-monospace text-decoration-none w-25 p-3 text-light" to="/about">About</Link>
+        class="navbar" to="/about">About</Link>
             <Link
-              class="fw-bold font-monospace text-decoration-none w-25 p-3 text-light" to="/promo"></Link>
-          </div>
-        </div>   
-      </nav>
-      <div class="container">
+        class="navbar" to="/promo"></Link>
+      </div>
           <Routes>
             <Route path="/" element={<Home/>}></Route>
             <Route path="/promo" element={<Promo />}></Route>
@@ -36,8 +31,6 @@ function App() {
             <Route path="/about" element={<About />}></Route>
           </Routes>
         </div>
-        </div>
-    </div>
   )
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,39 @@
 function Home() {
     return (
-    <div class="hello-home">'KopiKita'
-        <h5 class="hello">“dari aku, kopi dan kamu menjadi kita”</h5>
-        <img src="kopi3.png"></img>
+        <div class="body">
+            {/* <div class="hello-home">'KopiKita' */}
+            <img class="home-head" src="home-head.jpg" alt=""></img>
+            <div class="home">KopiKita</div>
+            <div class="line">|</div>
+            <div class="slider">
+            <div class="dariaku">"dari aku, kopi dan kamu menjadi kita"</div>
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="piscok.jpg" class="d-block w-100" alt="..."></img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="kopi.png" class="d-block w-100" alt="..."></img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="roti-bakar.jpg" class="d-block w-100" alt="..."></img>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+    </div>
     </div>
     )
 }
